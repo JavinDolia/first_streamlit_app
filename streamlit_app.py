19e6eb3e57c26eb288f1247c7446c720968563d5
JavinDolia <114331599+JavinDolia@users.noreply.github.com>
2022-10-01 19:15:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-streamlit.subtitle('orange and blueberry pancakes')
-streamlit.subtitle('Kale spinach and orange smoothie')
-streamlit.subtitle('Hard boiled free range eggs')
+streamlit.title('orange and blueberry pancakes')
+streamlit.title('Kale spinach and orange smoothie')
+streamlit.title('Hard boiled free range eggs')
