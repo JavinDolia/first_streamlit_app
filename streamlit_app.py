adeb01fd4178b13a609bdcabe3f747f238dbc7ac
JavinDolia <114331599+JavinDolia@users.noreply.github.com>
2022-10-01 19:18:50 +0530

    Update streamlit_app.py
    Updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('orange and blueberry pancakes')
-streamlit.text('Kale spinach and orange smoothie')
-streamlit.text('Hard boiled free range eggs')
+streamlit.text('🥣orange and blueberry pancakes')
+streamlit.text('🥗Kale spinach and orange smoothie')
+streamlit.text('🐔Hard boiled free range eggs')
+streamlit.text('🥑🍞Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
